3bedcbbcc18926e1185eed31144ae97ab3a0a949
Harris Putra <hputra@planatechnologies.com>
2025-12-29 14:44:51 +0800

    Trigger version update

--- TempFolderRemover/TempFolderRemover/Service1.cs
+++ TempFolderRemover/TempFolderRemover/Service1.cs
@@ -77,7 +77,7 @@ namespace TempFolderRemover
 
                 if (files.Length == 0)
                 {
-                    LogMessage($"No files found in {targetFolderPath}");
+                    LogMessage($"[Test QUEUE] No files found in {targetFolderPath}");
                     return;
                 }
 
