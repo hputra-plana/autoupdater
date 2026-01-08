c65bc778a4fc21565888b0c5f5ab652f441443ff
Harris Putra <hputra@planatechnologies.com>
2026-01-08 19:56:28 +0800

    Add OnBeforeInstall for fixed path

--- TempFolderRemover/TempFolderRemover/ProjectInstaller.cs
+++ TempFolderRemover/TempFolderRemover/ProjectInstaller.cs
@@ -6,6 +6,7 @@ using System.Configuration.Install;
 using System.Linq;
 using System.ServiceProcess;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TempFolderRemover
 {
@@ -34,6 +35,24 @@ namespace TempFolderRemover
             // Add installers to the collection
             Installers.Add(serviceProcessInstaller);
             Installers.Add(serviceInstaller);
+        }
+
+        protected override void OnBeforeInstall(IDictionary savedState)
+        {
+            base.OnBeforeInstall(savedState);
+
+            // Allow overriding install target via InstallUtil parameter: /installPath="C:\\autoupdaterpoc"
+            // Default to C:\autoupdaterpoc if not provided
+            string targetPath = Context?.Parameters?["installPath"];
+            if (string.IsNullOrWhiteSpace(targetPath))
+            {
+                targetPath = @"C:\\autoupdaterpoc";
+            }
+
+            // Force the service's binary path (binPath) to the desired install location
+            // Quote the path to support spaces
+            string exePath = Path.Combine(targetPath, "TempFolderRemover.exe");
+            Context.Parameters["assemblypath"] = "\"" + exePath + "\"";
         }
     }
 }
